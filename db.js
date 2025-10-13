8ad10077b5c40a19f38ca9d1b40c4b68efc7d8a0
shiroonigami23-ui <shiroonigami23@gmail.com>
2025-10-14 00:43:32 +0530

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-// =================================================================
-// DATABASE SETUP (Dexie.js)
-// =================================================================
-const db = new Dexie('myEnhancedNotesDB');
-
-db.version(1).stores({
-    notes: '++id, title, tags, isPinned, isLocked, createdAt, updatedAt',
-    settings: 'key' 
-});
